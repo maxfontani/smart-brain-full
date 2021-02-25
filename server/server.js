d44316ca66608d692d07bef48b2b2961bc712e89
Max Fontani <greengotty@gmail.com>
2021-02-25 23:52:01 +0200

    configured static server

--- server/server.js
+++ server/server.js
@@ -24,12 +24,23 @@ app.options('*', cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get('/', (req,res) => {res.send('The Smart Brain App is running')})
-app.post('/signin', (req,res) => handleSignin(req,res,db,bcrypt))
-app.post('/register', (req,res) => handleRegister(req,res,db,bcrypt))
-app.get('/profile/:id', (req,res) => handleProfile(req,res,db))
-app.put('/image', (req,res) => image.handleImage(req,res,db))
-app.post('/imageurl', (req,res) => image.handleApiCall(req,res))
+
+app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
+
+app.get('/api', function (req, res) {
+    res.set('Content-Type', 'application/json');
+    res.send('{"message":"Hello from the custom server!"}');
+  });
+
+app.get('*', function(request, response) {
+    response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
+  });
+
+app.post('/api/signin', (req,res) => handleSignin(req,res,db,bcrypt))
+app.post('/api/register', (req,res) => handleRegister(req,res,db,bcrypt))
+app.get('/api/profile/:id', (req,res) => handleProfile(req,res,db))
+app.put('/api/image', (req,res) => image.handleImage(req,res,db))
+app.post('/api/imageurl', (req,res) => image.handleApiCall(req,res))
 
 const PORT = process.env.PORT || 4000
 app.listen(PORT, () => console.log(`App is running on port ${PORT}`))
