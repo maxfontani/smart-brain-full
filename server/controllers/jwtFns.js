69dba8d89719523de1b665eb07af7581a74aabdf
Max Fontani <greengotty@gmail.com>
2021-03-31 15:25:15 +0300

    add auto-deploy

--- server/controllers/jwtFns.js
+++ server/controllers/jwtFns.js
@@ -3,6 +3,8 @@ import jwt from 'jsonwebtoken'
 export default function createSession(user) {
     const { id, login } = user
     const jwtPayload = { id, login }
-    const token = jwt.sign(jwtPayload, process.env.REACT_APP_JWT, {expiresIn: '1h'}) //MAKE PROCESS ENV
+    const token = jwt.sign(jwtPayload, process.env.REACT_APP_JWT, {expiresIn: '1h'}) 
+    console.log('ENV= ', process.env.REACT_APP_JWT)
+    console.log('token= ', token)
     return {status:"success", userId: id, token}
 }
